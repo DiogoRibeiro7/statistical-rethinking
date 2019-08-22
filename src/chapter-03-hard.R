c7f8fdaa558c746b27323fe44af3c04947b09448
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-08-21 21:04:41 -0400

    STY: rejigged code a bit

--- src/chapter-03-hard.R
+++ src/chapter-03-hard.R
@@ -26,17 +26,17 @@ abline(v=c(111, sum(birth1)), col=c("red", "blue"))
 dev.off()
 
 # 3H5
-boys_following_girls <- rep(0, 10000)
+boys_after_girls <- rep(0, 10000)
 for (i in 1:10000) {
   simulated_birth1 <- rbinom(100, 2, samples)
   simulated_birth2 <- rbinom(100, 2, samples)
-  boys_following_girls[i] <- sum(!simulated_birth1 & simulated_birth2)
+  boys_after_girls[i] <- sum(!simulated_birth1 & simulated_birth2)
 }
 
 # sum(!birth1 & birth2) = 39, which is so far right that it is not plotted!
 pdf("output/chapter-03.output-02.pdf")
-hist(boys_following_girls)
-abline(v=sum(!birth1 & birth2))
+hist(boys_after_girls)
+abline(v=sum(!birth1 & birth2), col="red")
 dev.off()
 
 # Conclusion: sexes of first and second births are correlated, which is not being modelled.
